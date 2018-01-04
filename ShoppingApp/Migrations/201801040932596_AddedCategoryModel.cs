a95e5feca27f55747f761ae9c8ae86121f21fdc6
ahashans <ahashans@gmail.com>
2018-01-04 15:33:53 +0600

    Added Category Model

---
+++ ShoppingApp/Migrations/201801040932596_AddedCategoryModel.cs
@@ -0,0 +1,26 @@
+namespace ShoppingApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedCategoryModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Categories");
+        }
+    }
+}
