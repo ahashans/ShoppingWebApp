77a6ed352250a934c2ed865f70ecf0e075a89b5d
ahashans <ahashans@gmail.com>
2018-01-04 15:30:40 +0600

    Added Image Model

--- ShoppingApp/Models/ApplicationDbContext.cs
+++ ShoppingApp/Models/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace ShoppingApp.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Image> Images { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ ShoppingApp/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoppingApp.Models
+{
+    public class Image
+    {
+        
+        public int ImageId { get; set; }
+        public string ImagePath { get; set; }
+
+
+    }
+}
