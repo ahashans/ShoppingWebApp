b4f651aef8d7cdebec58e40ba3cb1a7953e58154
ahashans <ahashans@gmail.com>
2018-01-04 19:49:34 +0600

    Completed CategoryModel With Image Insert Feature and All Category View Feature

---
+++ ShoppingApp/Controllers/CategoryController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using ShoppingApp.Models;
+using ShoppingApp.ViewModels;
+
+namespace ShoppingApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        private ApplicationDbContext _context;
+
+        public CategoryController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            var category = _context.Categories.ToList();
+            return View(category);
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CategoryViewModel
+            {
+                Category = new Category()
+            };
+            return View("CategoryForm",viewModel);
+        }
+        public ActionResult Save(CategoryViewModel categoryViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CategoryForm",categoryViewModel);
+            }
+            
+            string fileName = Path.GetFileNameWithoutExtension(categoryViewModel.ImageFileBase.FileName);
+            string extension = Path.GetExtension(categoryViewModel.ImageFileBase.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            categoryViewModel.Category.ImagePath = "~/Images/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/Images"),fileName);
+            categoryViewModel.ImageFileBase.SaveAs(fileName);
+            _context.Categories.Add(categoryViewModel.Category);
+            _context.SaveChanges();
+            ModelState.Clear();
+            var category = _context.Categories.ToList();
+            return View("Index",category);
+        }
+    }
+}
---
+++ ShoppingApp/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+using ShoppingApp.Models;
+
+namespace ShoppingApp.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public Category Category { get; set; }
+        public HttpPostedFileBase ImageFileBase { get; set; }
+    }
+}
